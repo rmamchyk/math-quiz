ae3a49ad005759ab3788badd97a320b3ed2c80a2
rmamchyk <roman.mamchyk@gmail.com>
2020-02-08 22:39:45 +0200

    added animation

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
-                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
+                <transition name="flip" mode="out-in">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'Question'"></component>
+                </transition>
             </div>
         </div>
     </div>
@@ -23,23 +23,48 @@
     export default {
         data() {
             return {
-                mode: 'app-question'
+                mode: 'Question'
             }
         },
         methods: {
           answered(isCorrect) {
               if (isCorrect) {
-                  this.mode = 'app-answer';
+                  this.mode = 'Answer';
               } else {
-                  this.mode = 'app-question';
+                  this.mode = 'Question';
                   alert('Wrong, try again!');
               }
           }
         },
         components: {
-            appQuestion: Question,
-            appAnswer: Answer
+            Question,
+            Answer
         }
     }
 </script>
+
+<style>
+.flip-enter-active {
+    animation: flip-in .5s ease-out forwards;
+}
+.flip-leave-active {
+    animation: flip-out .5s ease-out forwards;
+}
+@keyframes flip-out {
+    from {
+        transform: rotateY(0deg);
+    }
+    to {
+        transform: rotateY(90deg);
+    }
+}
+@keyframes flip-in {
+    from {
+        transform: rotateY(90deg);
+    }
+    to {
+        transform: rotateY(0deg);
+    }
+}
+</style>
 
